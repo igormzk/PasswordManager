37ea096ec7d9c864f3841cd613350f6c1aee2ff3
igormzk <60710182+igormzk@users.noreply.github.com>
2020-10-15 10:55:31 -0300

    Create Spot.java

---
+++ app/src/main/java/com/imeg/passwordmanager/model/Spot.java
@@ -0,0 +1,41 @@
+package com.imeg.passwordmanager.model;
+
+public class Spot {
+
+    private int id;
+    private String desc;
+    private String login;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

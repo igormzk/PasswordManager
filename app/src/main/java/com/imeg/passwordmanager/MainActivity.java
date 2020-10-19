a44fb4ab2f65cd3ff37f98eba8ef717a0a403bdb
igormzk <60710182+igormzk@users.noreply.github.com>
2020-10-19 17:19:06 -0300

    encrypt passwords

--- app/src/main/java/com/imeg/passwordmanager/MainActivity.java
+++ app/src/main/java/com/imeg/passwordmanager/MainActivity.java
@@ -1,28 +1,72 @@
 package com.imeg.passwordmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.imeg.passwordmanager.view.SpotActivity;
 import com.imeg.passwordmanager.view.SpotViewActivity;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
     private Button btnNewSpot;
     private Button btnViewSpot;
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = this;
+        btnViewSpot = findViewById(R.id.btnViewSpot);
         btnNewSpot = findViewById(R.id.btnNewSpot);
+        btnNewSpot.setEnabled(false);
+        btnViewSpot.setEnabled(false);
 
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(), "Erro na autenticação: " + errString, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                btnNewSpot.setEnabled(true);
+                btnViewSpot.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Autenticação correta!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Falha na autenticação.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Leitura biométrica para acesso")
+                .setSubtitle("Acesse usando sua digital").setNegativeButtonText("Use a senha").build();
+
+        biometricPrompt.authenticate(promptInfo);
+
+
+
+        context = this;
         btnNewSpot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnViewSpot = findViewById(R.id.btnViewSpot);
+
         btnViewSpot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/imeg/passwordmanager/db/SQLiteManager.java
+++ app/src/main/java/com/imeg/passwordmanager/db/SQLiteManager.java
@@ -6,11 +6,21 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Base64;
 
 import com.imeg.passwordmanager.model.Spot;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class SQLiteManager {
     private static final String DB_NAME="passwordmanager.db";
@@ -60,7 +70,7 @@ public class SQLiteManager {
             ContentValues cv = new ContentValues();
             cv.put("description", spot.getDescription());
             cv.put("login", spot.getLogin());
-            cv.put("password", spot.getPassword());
+            cv.put("password", encrypt(spot.getPassword()));
             result = dataBase.insert(TBL_SPOT,null, cv);
             dataBase.close();
         } catch (SQLException e) {
@@ -81,7 +91,7 @@ public class SQLiteManager {
             s.setId(c.getInt(c.getColumnIndex("id")));
             s.setDescription(c.getString(c.getColumnIndex("description")));
             s.setLogin(c.getString(c.getColumnIndex("login")));
-            s.setPassword(c.getString(c.getColumnIndex("password")));
+            s.setPassword(decrypt(c.getString(c.getColumnIndex("password"))));
             list.add(s);
             c.moveToNext();
         }
@@ -93,5 +103,44 @@ public class SQLiteManager {
     public long deleteSpot(int id) {
         dataBase = dbHelper.getWritableDatabase();
         return dataBase.delete(TBL_SPOT, "id = " + id, null);
+    }
+
+    private String encrypt(String text) {
+        try {
+            SecretKeySpec sks = new SecretKeySpec(getRaw("ImeG", "ResultText"), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, sks, new IvParameterSpec("664419738851919".getBytes()));
+            byte[] encrypted = cipher.doFinal(text.getBytes());
+            return Base64.encodeToString(encrypted, Base64.DEFAULT);
+        } catch (Exception e) {
+        }
+
+        return null;
+    }
+    private String decrypt(String text) {
+        try {
+            byte[] encrypted = Base64.decode(text, Base64.DEFAULT);
+
+            SecretKeySpec sks = new SecretKeySpec(getRaw("ImeG", "ResultText"), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, sks, new IvParameterSpec("664419738851919".getBytes()));
+            byte[] decrypted = cipher.doFinal(encrypted);
+            return new String(decrypted);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    private byte[] getRaw(String text, String alt) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            KeySpec ks = new PBEKeySpec(text.toCharArray(), alt.getBytes(), 10, 128);
+            return skf.generateSecret(ks).getEncoded();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }

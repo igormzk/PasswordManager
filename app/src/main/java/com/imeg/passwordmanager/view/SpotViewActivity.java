cdfdb6ee2313ffd5b0b4a5da62f36569568e166f
igormzk <60710182+igormzk@users.noreply.github.com>
2020-10-28 09:46:56 -0300

    list view with diferent colors

--- app/src/main/java/com/imeg/passwordmanager/view/SpotViewActivity.java
+++ app/src/main/java/com/imeg/passwordmanager/view/SpotViewActivity.java
@@ -1,15 +1,20 @@
 package com.imeg.passwordmanager.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.imeg.passwordmanager.R;
@@ -33,7 +38,20 @@ public class SpotViewActivity extends AppCompatActivity {
         final List<Spot> listSpot = sql.loadSpot();
         sql.close();
 
-        final ArrayAdapter<Spot> listAdapterSpot = new ArrayAdapter<Spot>(this, android.R.layout.simple_list_item_1, listSpot);
+        final ArrayAdapter<Spot> listAdapterSpot = new ArrayAdapter<Spot>(this, android.R.layout.simple_list_item_1, listSpot){
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = new TextView(context);
+                }
+                if (position % 2 == 0) {
+                    ((TextView)convertView).setBackgroundColor(Color.argb(255, 219,219,219));
+                }
+                return super.getView(position, convertView, parent);
+            }
+        };
 
         listView.setAdapter(listAdapterSpot);
 
